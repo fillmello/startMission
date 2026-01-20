6468dada7ada01eb7c1748c354046dc1b3685efd
Fill <fillmello@outlook.com>
2026-01-19 21:06:46 -0300

    Refactor code structure for improved readability and maintainability

--- startMission/src/App.tsx
+++ startMission/src/App.tsx
@@ -6,14 +6,6 @@ function App() {
   return (
     <div className="page" style={{ backgroundImage: `url(${banner})` }}>      
       <SubHeader />
-
-
-      <ul className="hero__bullets top-[60%]" aria-hidden="true">
-        <li className="hero__bullet hero__bullet--1"></li>
-        <li className="hero__bullet hero__bullet--2"></li>
-        <li className="hero__bullet hero__bullet--3"></li>
-        <li className="hero__bullet hero__bullet--4"></li>
-      </ul>
     </div>
   )
 }
--- startMission/src/SubHeader.tsx
+++ startMission/src/SubHeader.tsx
@@ -1,9 +1,9 @@
 import overlay from './assets/quadrado_branco@2x.png'
-import bannerOverlay from './assets/bannerHeaderOverlay.png'
 import quadradinhos from './assets/quadradinhos@2x.png'
 import aro1 from './assets/aro_1@2x.png'
 import aro2 from './assets/aro_2@2x.png'
 import bolas from './assets/bolas@2x.png'
+import pt from './assets/pt@2x.png'
 import "./SubHeader.css"
 import { Header } from './Header'
 
@@ -16,11 +16,17 @@ export function SubHeader(){
 
         <Header />
         <img src={overlay} alt="overlay" className="overlay__image" />
-        <img src={bannerOverlay} alt="banner overlay reference" className='z-7 banner__overlay' />
         <img src={quadradinhos} alt="quadradinhos" className='quadradinhos' />
         <img src={aro1} alt="aro 1" className='aro aro-1' />
         <img src={aro2} alt="aro 2" className='aro aro-2' />
+        <img src={pt} alt="pt" className='pt' />
         <img src={bolas} alt="bolas" className='bolas' />
+        <ul className="hero__bullets absolute left-[5.8cqi] top-[27.5cqi] z-50 grid gap-[2.6cqi]" aria-hidden="true">
+          <li className="hero__bullet w-2 h-2 rounded-full block bg-[#c74b3c]"></li>
+          <li className="hero__bullet w-2 h-2 rounded-full block bg-[#c74b3c]"></li>
+          <li className="hero__bullet w-2 h-2 rounded-full block bg-[#c74b3c]"></li>
+          <li className="hero__bullet w-2 h-2 rounded-full block bg-[#c74b3c]"></li>
+        </ul>
 
         <h1 className="hero__title">
             Assuma o seu
